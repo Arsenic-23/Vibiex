da9c358f759beeac8056927f06e823c146d35cca
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-03-26 21:44:28 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,37 @@
-helo 
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    telegramId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    joinedRooms: [
+        {
+            roomId: {
+                type: String,
+                required: true
+            },
+            joinedAt: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
