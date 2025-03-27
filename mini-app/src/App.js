79038e0b3d4818cd07e23ad3b4ab8e5098d6e47c
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-03-27 20:45:52 +0530

    Update App.js

--- mini-app/src/App.js
+++ mini-app/src/App.js
@@ -1,1 +1,36 @@
-g
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
+import Stats from "./pages/Stats";
+import Settings from "./pages/Settings";
+import Favorites from "./pages/Favorites";
+import History from "./pages/History";
+import Player from "./components/Player";
+import ThemeSettings from "./components/ThemeSettings";
+import WebSocketProvider from "./utils/websocket";
+
+function App() {
+  return (
+    <WebSocketProvider>
+      <Router>
+        <div className="app-container">
+          <Player />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/stats" element={<Stats />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/theme" element={<ThemeSettings />} />
+          </Routes>
+        </div>
+      </Router>
+    </WebSocketProvider>
+  );
+}
+
+export default App;
