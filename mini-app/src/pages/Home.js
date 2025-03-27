f9285b711de038be3d91ec05fe519844476e16d4
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-03-27 20:49:13 +0530

    Update Home.js

--- mini-app/src/pages/Home.js
+++ mini-app/src/pages/Home.js
@@ -1,1 +1,21 @@
-.
+import React, { useContext } from "react";
+import Player from "../components/Player";
+import Queue from "../components/Queue";
+import { WebSocketContext } from "../utils/websocket";
+
+function Home() {
+  const { participants } = useContext(WebSocketContext);
+
+  return (
+    <div className="home-page">
+      <header>
+        <h1>VIBIEX</h1>
+        <span className="participants">Listeners: {participants}</span>
+      </header>
+      <Player />
+      <Queue />
+    </div>
+  );
+}
+
+export default Home;
