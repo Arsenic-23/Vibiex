47b72b84daf86135a988d4390af896690544e252
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-03-27 17:26:46 +0530

    Delete mini-app directory

--- mini-app/src/config.js
+++
@@ -1,4 +0,0 @@
-const API_URL = "http://localhost:5000/api";  // Change this to deployed backend URL
-const BACKEND_WS_URL = "ws://localhost:5000";  // Change this to deployed WebSocket URL
-
-export { API_URL, BACKEND_WS_URL };
