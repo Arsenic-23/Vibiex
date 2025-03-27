6f0557f461e1551392b33329445b325f830b0333
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-03-27 20:50:18 +0530

    Update Stats.js

--- mini-app/src/pages/Stats.js
+++ mini-app/src/pages/Stats.js
@@ -1,1 +1,12 @@
-.
+import React from "react";
+import Stats from "../components/Stats";
+
+function StatsPage() {
+  return (
+    <div className="stats-page">
+      <Stats />
+    </div>
+  );
+}
+
+export default StatsPage;
